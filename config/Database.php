2fb6c4635af10858e17fa75fb3daeaccbd5a7b82
Mcfly6 <manu.mcfly6@gmail.com>
2020-11-14 11:35:07 -0300

    setting database

--- config/Database.php
+++ config/Database.php
@@ -1,39 +1,36 @@
 <?php
 class Database
 {
+    private $db = parse_url(getenv("postgresql-octagonal-09743"));
 
-    private $host='localhost';
-    private $db_name='products';
-    private $user='postgres';
-    private $password='1234';
+    // private $host='localhost';
+    // private $db_name='products';
+    // private $user='postgres';
+    // private $password='1234';
     private $conn;
 
-    public function connect(){
+    public function connect()
+    {
 
-        $this->conn=null;
+        $this->conn = null;
 
-        try{
-            $this->conn=new PDO(
-                'pgsql:host='. $this->host .';dbname='. $this->db_name,$this->user,$this->password
+        try {
+            $this->conn = new PDO(
+                "pgsql:" . sprintf(
+                    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+                    $this->db["host"],
+                    $this->db["port"],
+                    $this->db["user"],
+                    $this->db["pass"],
+                    ltrim($this->db["path"], "/")
+                )
             );
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
 
-
-        }catch(PDOException $e){
-
-            echo 'Connection Error: '. $e->getMessage();
-
-
-
+            echo 'Connection Error: ' . $e->getMessage();
         }
 
         return $this->conn;
-    
     }
-
-
 }
-
-
-
-?>
