57e388e756773010514bcaa2cfcb3c06cf2cba8f
Mcfly6 <manu.mcfly6@gmail.com>
2020-11-14 20:14:01 -0300

    test

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding,Origin,X-Requested-width,Accept');
 header('Content-type:application/json;charset=utf-8');
 
-require 'vendor/autoload.php';
+// require 'vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable( __DIR__);
 $dotenv->load();
