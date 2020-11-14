9b2d38e017e84363056929bf1c7edad81e1a2f9f
Mcfly6 <manu.mcfly6@gmail.com>
2020-11-14 19:52:27 -0300

    test

--- api/check-cart/index.php
+++ api/check-cart/index.php
@@ -3,10 +3,10 @@
 require  "../../index.php";
 
 
-header('Access-Control-Allow-Origin: https://ecommerce-client-test.herokuapp.com/');
+header('Access-Control-Allow-Origin: https://ecommerce-client-test.herokuapp.com');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding");
-header("Content-type:application/json");
+header("Content-type:application/json, multipart/form-data");
 
 
 
@@ -16,7 +16,7 @@ if (isset($_POST['checkCart']) && isset($_POST['checkTotal'])) {
     $debt = $_POST['checkTotal'];
     $api_url = 'https://staging.adamspay.com/api/v1/debts?update_if_exists=1';
     $api_key= $_ENV['API_KEY']; //GET ENV
-    echo "hello im not broke"
+    echo "hello im not broke";
 
     //the true 2d arg allow to use as an array
     $cart_info = json_decode($cart_raw, true);
