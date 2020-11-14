65925d3f2dc6557d6efea195bc6da417af539a43
Mcfly6 <manu.mcfly6@gmail.com>
2020-11-14 20:03:43 -0300

    fix res to client

--- api/check-cart/index.php
+++ api/check-cart/index.php
@@ -5,7 +5,7 @@ require  "../../index.php";
 
 header('Access-Control-Allow-Origin: https://ecommerce-client-test.herokuapp.com');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding");
+header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding,Origin,X-Requested-width,Accept");
 header("Content-type:application/json, multipart/form-data");
 
 echo "hello im not broke";
