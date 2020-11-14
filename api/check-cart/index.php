a3f36826eb24ad5c57ef90c947bb285fff3fa743
Mcfly6 <manu.mcfly6@gmail.com>
2020-11-14 19:58:54 -0300

    test

--- api/check-cart/index.php
+++ api/check-cart/index.php
@@ -8,7 +8,7 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding");
 header("Content-type:application/json, multipart/form-data");
 
-
+echo "hello im not broke";
 
 if (isset($_POST['checkCart']) && isset($_POST['checkTotal'])) {
 
@@ -16,7 +16,7 @@ if (isset($_POST['checkCart']) && isset($_POST['checkTotal'])) {
     $debt = $_POST['checkTotal'];
     $api_url = 'https://staging.adamspay.com/api/v1/debts?update_if_exists=1';
     $api_key= $_ENV['API_KEY']; //GET ENV
-    echo "hello im not broke";
+    
 
     //the true 2d arg allow to use as an array
     $cart_info = json_decode($cart_raw, true);
