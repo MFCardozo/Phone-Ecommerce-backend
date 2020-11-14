faada890e3f7782686f52f5bd494b1d91ae9a617
Mcfly6 <manu.mcfly6@gmail.com>
2020-11-14 19:47:00 -0300

    test

--- api/check-cart/index.php
+++ api/check-cart/index.php
@@ -3,7 +3,7 @@
 require  "../../index.php";
 
 
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: https://ecommerce-client-test.herokuapp.com/');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding");
 header("Content-type:application/json");
