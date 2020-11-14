36466268954fc8025d91c131883c1cd2f6bba685
Mcfly6 <manu.mcfly6@gmail.com>
2020-11-14 20:18:29 -0300

    test

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header('Access-Control-Allow-Origin: https://ecommerce-client-test.herokuapp.com');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding,Origin,X-Requested-width,Accept');
 header('Content-type:application/json;charset=utf-8');
 
-// require 'vendor/autoload.php';
+require 'vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable( __DIR__);
 $dotenv->load();
