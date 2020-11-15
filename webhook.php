88d5e530fdd70a9e82c942b6b523c6ec5de22a16
Mcfly6 <manu.mcfly6@gmail.com>
2020-11-15 02:02:42 -0300

    connect webhook

--- webhook.php
+++ webhook.php
@@ -1,3 +1,17 @@
-$payload = file_get_contents("php://input");
+<?php
 
-echo $payload;
+$post = file_get_contents('php://input'); // el POST
+$secret = $_ENV['API_SECRET']; // Obtener del UI de administración
+ 
+$hmac_req = md5( 'adams' . $post . $secret );
+$hmac_rec = @$_SERVER['HTTP_X_ADAMS_NOTIFY_HASH'];
+ 
+if( $hmac_req !== $hmac_rec ){
+  die('Validación ha fallado'); // Ignorar esta notificación
+}
+
+
+// Todo OK: Procesar
+echo $post;
+
+?>
