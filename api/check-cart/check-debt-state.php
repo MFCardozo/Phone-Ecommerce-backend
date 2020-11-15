76e74b54d36536f46c1699850b4aed108df69d23
Mcfly6 <manu.mcfly6@gmail.com>
2020-11-15 11:52:42 -0300

    test payment3

--- api/check-cart/check-debt-state.php
+++ api/check-cart/check-debt-state.php
@@ -5,5 +5,48 @@ header('Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content
 header('Content-type:application/json;charset=utf-8');
 
 $debt_id = isset($_GET['debt-id']) ? $_GET['debt-id'] : die();
-echo "working perfect " . $debt_id;
+
+$api_url = 'https://staging.adamspay.com/api/v1/debts/' . $debt_id;
+$api_key= $_ENV['API_KEY']; //GET ENV
+
+$curl = curl_init();
+ 
+curl_setopt_array($curl,[
+ CURLOPT_URL => $apiUrl,
+ CURLOPT_HTTPHEADER => ['apikey: '.$apiKey],
+ CURLOPT_RETURNTRANSFER => true
+ ]);
+ 
+$response = curl_exec($curl);
+if( $response ){
+  $data = json_decode($response,true);
+ 
+  // Verificar estado de pago
+  $debt = isset($data['debt']) ? $data['debt'] : null;
+  if( $debt ){
+    
+    
+    $payStatus = $debt['payStatus']['status'];
+    
+    $isPaid =$payStatus === 'paid';
+ 
+    
+    if( $isPaid ){
+      
+      echo "Thanks for purchasing,we wait for you soon";
+    }
+    else {
+      echo "Pay canceled successfully";
+    }
+ 
+  } else {
+    echo "cannot get the pay status\n";
+    print_r($data['meta']);
+  }
+ 
+}
+else {
+  echo 'curl_error: ',curl_error($curl);
+}
+curl_close($curl);
 ?>
