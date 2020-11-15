dccb62512eaf8ce2a42f15d98175220b244f8631
Mcfly6 <manu.mcfly6@gmail.com>
2020-11-15 10:58:20 -0300

    test payment

--- api/check-cart/check-debt-state.php
+++ api/check-cart/check-debt-state.php
@@ -1,4 +1,8 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding,Origin,X-Requested-width,Accept');
+header('Content-type:application/json;charset=utf-8');
 
 $debt_id = isset($_GET['debt-id']) ? $_GET['debt-id'] : die();
 echo $debt_id
